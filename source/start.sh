f888ed80d6456ae8bf24ce9ada5882af45d23c1a
mayel.espino <mayel.espino@gmail.com>
2024-11-14 21:51:51 +0000

    updated start.sh to use pinggy.link tunnel.

--- source/start.sh
+++ source/start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-ngrok start home --config ./ngrok.yaml > /dev/null &
-
+# /snap/bin/ngrok start home --config /home/pi/gate-pi/source/ngrok.yaml > /dev/null &
+ssh -p 443 -o StrictHostKeyChecking=no -o ServerAliveInterval=30 -R0:localhost:8000 a.pinggy.io
