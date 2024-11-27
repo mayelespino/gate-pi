974b5b21a221f16021faa59b0d2de1fabd7c5a95
mayel.espino <mayel.espino@gmail.com>
2024-11-27 04:42:06 +0000

    Upated index.php on gatepi.

--- web_gate/index.php
+++ web_gate/index.php
@@ -19,7 +19,6 @@ echo $result;
 $output = file_get_contents('http://sensor.local');
 echo $output;
 ?>
-<br/>
 
 <?php
 	$url = $_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
@@ -33,14 +32,54 @@ echo $output;
 ?>
 
 
-<br/>
+<?php
+	$url = $_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+	if ($url == "gate.local/") {
+		echo "<a href=\"http://gate.local:3000\">[graphana]</a>";
+	} else {
+		echo "[graphana]";
+	}
+	$output = file_get_contents('http://speaker.local');
+	echo $output;
+?>
+
+
 <a href="bookmarks.php">[bookmarks]</a>
-<br/>
+
 <a href="http://sensor.local:667" target="_blank">[darkstat]</a>
-<br/>
+
 <a href="http://sensor.local:3000" target="_blank">[ntopng]</a>
+
 <hr/>
 
+<br/>
+
+<?php
+	$output = shell_exec('ping -c1 sensor.local');
+	echo "<h3>sensorpi</h3><pre>$output</pre><br/>";
+
+	$output = shell_exec('ping -c1 speaker.local');
+	echo "<h3>speaker</h3><pre>$output</pre><br/>";
+
+	$output = shell_exec('ping -c1 supermicro');
+	echo "<h3>supermicro</h3><pre>$output</pre><br/>";
+
+	$output = shell_exec('ping -c1 super2');
+	echo "<h3>super2</h3><pre>$output</pre><br/>";
+?>
+<?php
+	$output = shell_exec('ping -c1 antsle.local');
+	echo "<h3>antsle</h3><pre>$output</pre><br/>";
+
+	$output = shell_exec('ping -c1 chaperon.local');
+	echo "<h3>chaperon</h3><pre>$output</pre><br/>";
+
+	$output = shell_exec('ping -c1 Mac-mini-2.lan');
+	echo "<h3>Mac-mini</h3><pre>$output</pre><br/>";
+
+?>
+
+<br/>
 <H2>Links</H2>
 
 <a href='https://www.sf72.org'>SF72 - San Francisco </a><br/>
